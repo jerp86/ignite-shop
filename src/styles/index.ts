081acf4a20eabd94a4240a215d75f28f9960546a
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-21 00:07:55 -0300

    add colors and font-sizes

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -13,6 +13,23 @@ export const {
   theme: {
     colors: {
       white: '#FFF',
+
+      gray900: '#121214',
+      gray800: '#202024',
+      gray300: '#C4C4C4',
+      gray100: '#E1E1E6',
+
+      green500: '#00875F',
+      green300: '#00B37E',
+
+      backgroundGradient: 'linear-gradient(180deg, #1EA483 0%, #7465D4 100%)',
+    },
+
+    fontSizes: {
+      md: '1.8rem',
+      lg: '2rem',
+      xl: '2.4rem',
+      '2xl': '3.2rem',
     },
   },
 })
