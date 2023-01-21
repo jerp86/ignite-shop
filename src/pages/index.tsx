d5382a84570ed48d205852cab90ecfaad3b8dad2
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-21 01:46:05 -0300

    create home styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,47 @@
+import { HomeContainer, Product } from '@/styles/pages/home'
+import Image from 'next/image'
+
+import camiseta1 from '@/assets/camisetas/1.png'
+import camiseta2 from '@/assets/camisetas/2.png'
+import camiseta3 from '@/assets/camisetas/3.png'
+import camiseta4 from '@/assets/camisetas/4.png'
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta Beyond the Limits</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta Beyond the Limits</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta3} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta Beyond the Limits</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta4} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta Beyond the Limits</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  )
 }
---
+++ src/styles/pages/home.ts
@@ -0,0 +1,62 @@
+import { styled } from '..'
+
+export const HomeContainer = styled('main', {
+  display: 'flex',
+  width: '100%',
+  maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
+  marginLeft: 'auto',
+  minHeight: 656,
+})
+
+export const Product = styled('a', {
+  background: '$backgroundGradient',
+  borderRadius: 8,
+  position: 'relative',
+  overflow: 'hidden',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  img: {
+    objectFit: 'cover',
+  },
+
+  footer: {
+    position: 'absolute',
+    bottom: '0.4rem',
+    left: '0.4rem',
+    right: '0.4rem',
+    padding: '3.2rem',
+
+    borderRadius: 6,
+
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+
+    transform: 'translateY(110%)',
+    opacity: 0,
+    transition: 'all 0.2s ease-in-out',
+
+    strong: {
+      fontSize: '$lg',
+      color: '$gray100',
+    },
+
+    span: {
+      fontSize: '$xl',
+      fontWeight: 'bold',
+      color: '$green300',
+    },
+  },
+
+  '&:hover': {
+    footer: {
+      transform: 'translateY(0%)',
+      opacity: 1,
+    },
+  },
+})
