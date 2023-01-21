2c7c7dad700338eb84308174df08c2664c70b0b3
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-21 00:35:24 -0300

    add styles and asset

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
 
+import logoImg from '@/assets/logo.svg'
+import { AppContainer, AppHeader } from '@/styles/pages/app'
+
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AppContainer>
+      <AppHeader>
+        <img src={logoImg.src} alt="" />
+      </AppHeader>
+
+      <Component {...pageProps} />
+    </AppContainer>
+  )
 }
