474891f834469fd6257c1bb3c42af8f8ef90b267
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-21 00:34:52 -0300

    create app styles

---
+++ src/styles/pages/app.ts
@@ -0,0 +1,16 @@
+import { styled } from '..'
+
+export const AppContainer = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  justifyContent: 'center',
+  minHeight: '100vh',
+})
+
+export const AppHeader = styled('header', {
+  padding: '3.2rem 0',
+  width: '100%',
+  maxWidth: 1180,
+  margin: '0 auto',
+})
