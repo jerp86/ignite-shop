d2bbda5d655da7558c953837d863cf3dd7d2e219
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-23 12:09:42 -0300

    add getServerSideProps and props

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,23 +1,63 @@
+import { stripe } from '@/lib/stripe'
 import { SuccessImageContainer, SuccessContainer } from '@/styles/pages/success'
+import { GetServerSideProps } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
+import Stripe from 'stripe'
 
-export default function Success() {
+interface SuccessProps {
+  costumerName: string
+  product: {
+    name: string
+    imageUrl: string
+  }
+}
+
+export default function Success({ costumerName, product }: SuccessProps) {
   return (
     <SuccessContainer>
       <h1>Compra Efetuada</h1>
 
       <SuccessImageContainer>
-        <Image src="" alt="" width={120} height={110} />
+        <Image src={product.imageUrl} alt="" width={120} height={110} />
       </SuccessImageContainer>
 
       <p>
-        Uhuul <strong>José Eduardo</strong>, sua{' '}
-        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
-        casa.
+        Uhuul <strong>{costumerName}</strong>, sua{' '}
+        <strong>{product.name}</strong> já está a caminho da sua casa.
       </p>
 
       <Link href="/">Voltar ao catálogo</Link>
     </SuccessContainer>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  if (!query.session_id) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
+
+  const sessionId = String(query.session_id)
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ['line_items', 'line_items.data.price.product'],
+  })
+
+  const costumerName = session.customer_details?.name
+  const product = session.line_items?.data[0].price?.product as Stripe.Product
+
+  return {
+    props: {
+      costumerName,
+      product: {
+        name: product.name,
+        imageUrl: product.images[0],
+      },
+    } as SuccessProps,
+  }
 }
