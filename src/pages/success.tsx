a554a5b7ca39cba063c6bcd58d5ea82c1110d3d5
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-23 11:50:59 -0300

    add structure

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,23 @@
+import { SuccessImageContainer, SuccessContainer } from '@/styles/pages/success'
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default function Success() {
-  ;<h1>Success</h1>
+  return (
+    <SuccessContainer>
+      <h1>Compra Efetuada</h1>
+
+      <SuccessImageContainer>
+        <Image src="" alt="" width={120} height={110} />
+      </SuccessImageContainer>
+
+      <p>
+        Uhuul <strong>José Eduardo</strong>, sua{' '}
+        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
+        casa.
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  )
 }
