ec8816e39d1387cb3c71b4cfcab8a50243c4ae00
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-22 22:24:10 -0300

    add structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,34 @@
+import Image from 'next/image'
 import { useRouter } from 'next/router'
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '@/styles/pages/product'
 
 export default function Product() {
   const { query } = useRouter()
+  console.log(query)
 
-  return <h1>Product {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer>
+        <Image src="" alt="" />
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit,
+          ratione nam odio ducimus id accusamus unde voluptatem sit perspiciatis
+          eos, ea sunt blanditiis molestias, nulla aperiam aspernatur fugiat
+          delectus maiores.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
