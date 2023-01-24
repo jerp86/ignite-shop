3dbe16ec1cab2e5d44ddefcbaf9540484c0283b5
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-24 11:52:44 -0300

    add colors and fix gray300

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -16,13 +16,16 @@ export const {
 
       gray900: '#121214',
       gray800: '#202024',
-      gray300: '#C4C4C4',
+      gray500: '#8D8D99',
+      gray300: '#C4C4CC',
       gray100: '#E1E1E6',
 
       green500: '#00875F',
       green300: '#00B37E',
 
       backgroundGradient: 'linear-gradient(180deg, #1EA483 0%, #7465D4 100%)',
+      skeleton:
+        'linear-gradient(90deg, $gray800 0%, rgba(18, 18, 20, 0.75) 100%)',
     },
 
     fontSizes: {
