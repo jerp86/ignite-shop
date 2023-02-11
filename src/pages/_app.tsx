35607f0c706d1e30931e8ebb4f732088ddc95602
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-02-11 01:48:55 -0300

    refactor: add Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,14 @@
+import { Header } from '@/components'
 import { globalStyles } from '@/styles/global'
-import { AppContainer, AppHeader } from '@/styles/pages/app'
+import { AppContainer } from '@/styles/pages/app'
 import type { AppProps } from 'next/app'
-import Image from 'next/image'
-
-import logoImg from '@/assets/logo.svg'
-import Link from 'next/link'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppContainer>
-      <AppHeader>
-        <Link href="/" prefetch={false}>
-          <Image src={logoImg} alt="" />
-        </Link>
-      </AppHeader>
+      <Header />
 
       <Component {...pageProps} />
     </AppContainer>
