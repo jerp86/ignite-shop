0b7efa2163475ebe4b7d9e5da4c3288ac84f290c
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-02-13 17:25:14 -0300

    feat (api): update prop of body and line_items

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -1,3 +1,4 @@
+import { ProductProps } from '@/context/CartContext'
 import { stripe } from '@/lib/stripe'
 import { NextApiRequest, NextApiResponse } from 'next'
 
@@ -5,14 +6,14 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const { priceId } = req.body
+  const { products } = req.body as { products: ProductProps[] }
 
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' })
   }
 
-  if (!priceId) {
-    return res.status(400).json({ error: 'Price not found' })
+  if (!products || products.length < 1) {
+    return res.status(400).json({ error: 'Products not found' })
   }
 
   const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`
@@ -22,12 +23,10 @@ export default async function handler(
     success_url: successUrl,
     cancel_url: cancelUrl,
     mode: 'payment',
-    line_items: [
-      {
-        price: priceId,
-        quantity: 1,
-      },
-    ],
+    line_items: products.map(({ defaultPriceId }) => ({
+      price: defaultPriceId,
+      quantity: 1,
+    })),
   })
 
   return res.status(201).json({ checkoutUrl: checkoutSession.url })
